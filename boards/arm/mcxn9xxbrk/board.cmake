edfc59286f2dea0d49b9bf4f70312a495b82d1c2
Mahesh Mahadevan <mahesh.mahadevan@nxp.com>
2023-04-17 09:36:30 -0500

    boards: mcxn9xxbrk: Update SoC Name provided to JLINK
    Update the SoC Name used by JLink
    
    Signed-off-by: Mahesh Mahadevan <mahesh.mahadevan@nxp.com>

--- boards/arm/mcxn9xxbrk/board.cmake
+++ boards/arm/mcxn9xxbrk/board.cmake
@@ -4,6 +4,6 @@
 # SPDX-License-Identifier: Apache-2.0
 #
 
-board_runner_args(jlink "--device=MCXN947" "--reset-after-load")
+board_runner_args(jlink "--device=MCXN947_M33_0" "--reset-after-load")
 
 include(${ZEPHYR_BASE}/boards/common/jlink.board.cmake)
