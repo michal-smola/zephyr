40c4b5b302efa515f8c7aac37f5485e934c25c0b
Yves Vandervennet <yves.vandervennet@nxp.com>
2023-12-11 10:23:49 -0600

    mcxn9xxbrk: linkserver: add support for cpu0
    usual commands debug, flash supported
    support for cpu1 to follow once runtime code is enabled
    
    Signed-off-by: Yves Vandervennet <yves.vandervennet@nxp.com>

--- boards/arm/mcxn9xxbrk/board.cmake
+++ boards/arm/mcxn9xxbrk/board.cmake
@@ -4,6 +4,14 @@
 # SPDX-License-Identifier: Apache-2.0
 #
 
+if(CONFIG_SOC_MCX_N947_CPU0)  ## OR CONFIG_SECOND_CORE_MCUX?
 board_runner_args(jlink "--device=MCXN947_M33_0" "--reset-after-load")
+board_runner_args(linkserver  "--device=MCXN947:MCX-N9XX-BRK:cm33_core0")
+board_runner_args(linkserver  "--override=/device/memory/5/flash-driver=MCXN9xx_S.cfx")
+board_runner_args(linkserver  "--override=/device/memory/5/location=0x10000000")
+else()
+message(FATAL_ERROR "Support for cpu1 not available yet")
+endif()
 
 include(${ZEPHYR_BASE}/boards/common/jlink.board.cmake)
+include(${ZEPHYR_BASE}/boards/common/linkserver.board.cmake)
