a9638601ae31c3b57cfb757b3a41fb6d8015b0cf
Derek Snell <derek.snell@nxp.com>
2024-01-08 13:03:02 -0500

    boards: arm: frdm_mcxn947: Make LinkServer default runner
    Also changes board part number for LinkServer
    
    Signed-off-by: Derek Snell <derek.snell@nxp.com>

--- boards/arm/frdm_mcxn947/board.cmake
+++ boards/arm/frdm_mcxn947/board.cmake
@@ -6,7 +6,7 @@
 
 if(CONFIG_SOC_MCX_N947_CPU0)  ## OR CONFIG_SECOND_CORE_MCUX?
 board_runner_args(jlink "--device=MCXN947_M33_0" "--reset-after-load")
-board_runner_args(linkserver  "--device=MCXN947:MCX-N9XX-BRK:cm33_core0")
+board_runner_args(linkserver  "--device=MCXN947:MCX-N9XX-EVK:cm33_core0")
 board_runner_args(linkserver  "--override=/device/memory/1/flash-driver=MCXN9xx_S.cfx")
 board_runner_args(linkserver  "--override=/device/memory/1/location=0x10000000")
 else()
@@ -14,5 +14,5 @@ message(FATAL_ERROR "Support for cpu1 not available yet")
 endif()
 
 
-include(${ZEPHYR_BASE}/boards/common/jlink.board.cmake)
 include(${ZEPHYR_BASE}/boards/common/linkserver.board.cmake)
+include(${ZEPHYR_BASE}/boards/common/jlink.board.cmake)
