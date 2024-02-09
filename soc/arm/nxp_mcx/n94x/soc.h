ebc76be409302c92e4391b0d8251e8822ed54eda
Mahesh Mahadevan <mahesh.mahadevan@nxp.com>
2024-02-09 16:28:08 -0600

    soc: mcx: Update soc.h
    Add a prototype for the flash get rate function
    
    Signed-off-by: Mahesh Mahadevan <mahesh.mahadevan@nxp.com>

--- soc/arm/nxp_mcx/n94x/soc.h
+++ soc/arm/nxp_mcx/n94x/soc.h
@@ -7,12 +7,23 @@
 #ifndef _SOC__H_
 #define _SOC__H_
 
+#ifndef _ASMLANGUAGE
+
 #include <fsl_port.h>
+#include <fsl_common.h>
 
 #define PORT_MUX_GPIO kPORT_MuxAlt0 /* GPIO setting for the Port Mux Register */
 
-#ifndef _ASMLANGUAGE
-#include <fsl_common.h>
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+uint32_t flexspi_clock_set_freq(uint32_t clock_name, uint32_t rate);
+
+#ifdef __cplusplus
+}
+#endif
+
 #endif /* !_ASMLANGUAGE */
 
 #endif /* _SOC__H_ */
